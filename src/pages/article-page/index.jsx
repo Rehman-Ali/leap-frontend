435009bd7fdc201379ca8951e4be5a053ec8073b
Rehman Ali <softdix@gmail.com>
2025-01-31 20:33:06 +0500

    update code

--- src/pages/article-page/index.jsx
+++ src/pages/article-page/index.jsx
@@ -1,18 +1,45 @@
 "use client";
 import { useInView } from "@/hooks/useInView";
+import { SERVER_URL } from "@/utils/server";
+import axios from "axios";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const ArticleScreen = () => {
   const sectionRef = useRef(null); // Ref for a div element
   const isVisible = useInView(sectionRef, { threshold: 0.4 });
   const [animationTriggered, setAnimationTriggered] = useState(false);
-
+  const [articleList, setArticleList] = useState([]);
   // Trigger animation only once
   if (isVisible && !animationTriggered) {
     setAnimationTriggered(true);
   }
+
+  // const getArticleList = (token) => {
+  //   const response = axios.get(`${SERVER_URL}/api/article/all`, {
+  //     headers: {
+  //       "x-auth-token": token
+  //     }
+  //   });
+  //   setArticleList(response.data.data);
+  // };
+
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("u_t"));
+    axios.get(`${SERVER_URL}/api/article/all`, {
+      headers: {
+        "x-auth-token": token
+      }
+    })
+    .then(res => setArticleList(response.data.data ))
+    .catch(err => console.log(err, "err is here==="))
+    
+    // getArticleList();
+  }, []);
+
+  console.log(articleList, "articleList=====");
+
   return (
     <div ref={sectionRef} className="bg-bodycolor max-w-[1200px] mx-auto">
       <div className="flex flex-row items-center justify-center">
@@ -57,23 +84,45 @@ const ArticleScreen = () => {
         </div>
       </div>
       <div className="flex flex-row flex-wrap gap-4">
-        <div className="bg-gray-500 h-[300px] w-[24%] rounded-[8px]">
-          <Image
-            src="/assets/articles/pic1.jpg"
-            height={157}
-            width={300}
-            className="h-[157px] w-[100%] rounded-t-[8px]"
-          />
-          <div className="p-[10px] h-[143px] bg-white flex flex-col justify-between rounded-b-[8px]">
-            <div className="flex flex-col gap-y-2">
-              <p className="text-red-700 font-inter text-[16px] font-medium">STOCK</p>
-              <p className="text-black font-inter font-medium text-[16px]">Article Name</p>
+        {articleList.length > 0 ? (
+          articleList.map((item, index) => (
+            <div
+              className="bg-gray-500 h-[300px] w-[24%] rounded-[8px]"
+              key={index}
+            >
+              <Image
+                src={item.image_url}
+                height={157}
+                width={300}
+                className="h-[157px] w-[100%] rounded-t-[8px]"
+              />
+              <div className="p-[10px] h-[143px] bg-white flex flex-col justify-between rounded-b-[8px]">
+                <div className="flex flex-col gap-y-2">
+                  <p className="text-red-700 font-inter text-[16px] font-medium">
+                    {item.category}
+                  </p>
+                  <p className="text-black font-inter font-medium text-[16px]">
+                    {item.title}
+                  </p>
+                </div>
+                <p className="text-black font-inter font-normal text-[12px]">
+                  By&nbsp;
+                  <span className="text-black font-semibold text-[14px]">
+                    &nbsp;
+                    {item.written_by}
+                  </span>
+                  &nbsp; | item
+                </p>
+              </div>
             </div>
-            <p className="text-black font-inter font-normal text-[12px]">
-             By <span className="text-black font-semibold text-[14px]"> Author Name</span> | 20-20-2020
+          ))
+        ) : (
+          <div className="flex flex-row items-center justify-center">
+            <p className="text-white font-inter font-medium text-[18px] text-center">
+              No Article Found
             </p>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
