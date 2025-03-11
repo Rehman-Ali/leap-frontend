1643026dac9647aac1f04b87686f739c5431d538
Rehman Ali <softdix@gmail.com>
2025-03-11 16:27:51 +0500

    Update page.jsx

--- src/app/update-article/page.jsx
+++ src/app/update-article/page.jsx
@@ -2,15 +2,6 @@
 import UpdateArticleScreen from "@/pages/Dashboard/articles/update-article";
 import { useSearchParams } from 'next/navigation';
 
-export const metadata = {
-  title: "Leap",
-  description: "",
-  keywords: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
-  other: {
-    "google-site-verification": "KtTMBV0PsMKme0FfaQTOlUYnMc4y7jTfcKHDXF_4CzU",
-  },
-};
-
 
 
 const ArticleUpdatePage = () => {
